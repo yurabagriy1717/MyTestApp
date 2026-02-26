bc2c735f91bad1b7a9b201e047d7977cff80932e
Yura <yurabahrii17@gmail.com>
2026-02-26 14:51:38 +0100

    setup project

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by Юрій Багрій on 26.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
